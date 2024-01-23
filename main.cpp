6abd304f23f198abfaaa5668bc27711478038f7a
hellokms8828 <hellokms8828@naver.com>
2024-01-24 01:43:10 +0900

    Fix: syntax

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 void usage()
 {
-    printf("syntax: deauth <interface> <ap mac> [<station mac>]\n");
+    printf("syntax: deauth <interface> <ap mac> [<station mac> -auth]\n");
     printf("sample: deauth mon0 00:11:22:33:44:55 66:77:88:99:AA:BB\n");
 }
 
